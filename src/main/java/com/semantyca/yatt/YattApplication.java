42b4f36740cf52d5444a5e63c974006cda7572ba
kaira.kuliyeva <kaira.kuliyeva@accenture.com>
2020-03-23 17:34:21 +0200

    #

---
+++ src/main/java/com/semantyca/yatt/YattApplication.java
@@ -0,0 +1,13 @@
+package com.semantyca.yatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semantyca/yatt/YattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semantyca.yatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
